156b1d173b70750502d5921c8571c11cb0f0e06e
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-09-24 13:38:39 -0400

    commit tree bf329c894e7c7040ca32dc2473d2fd9534e600aa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return array;
+}
+
+function countDown() {
+    let count = 0;
+    while (count < 11) {
+        console.log(count++);
+    }
+   return count; 
+}
